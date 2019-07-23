054a45eb34e94e75efc8093c93cc3071f655fdf9
bh <bwhellar@gmail.com>
2019-07-23 14:36:34 -0700

    bring in an api for testing purposes

--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 
 export class PersonsService {
   personsChanged = new Subject<string[]>();
 
-  public persons: string[] = ['Brass', 'Lyra', 'Gishath', 'Goreclaw', 'Neheb', 'Yuriko'];
+  persons: string[];
+
+  constructor(private http: HttpClient) {}
+
+    fetchPersons() {
+      this.http
+      .get<any>('https://swapi.co/api/people')
+      .pipe(map(resData => {
+        return resData.results.map(character => character.name);
+      }))
+      .subscribe(transData => {
+        this.personsChanged.next(transData);
+      });
+    }
+
 
   addPerson(name: string){
     this.persons.push(name);
