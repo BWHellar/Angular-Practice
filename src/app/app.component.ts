a655f09d23450db7c7f7574598652ff32758a202
bh <bwhellar@gmail.com>
2019-07-23 11:32:27 -0700

    add string interp for the names included

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngFresh';
+  persons: string[] = ['Brass', 'Lyra', 'Gishath', 'Goreclaw', 'Neheb', 'Yuriko'];
 }
