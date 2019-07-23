31746aa918a689906ea1700090219b49001ad408
bh <bwhellar@gmail.com>
2019-07-23 11:43:37 -0700

    Add an input option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { PersonInputComponent } from './persons/person-input.component';
 import { AppComponent } from './app.component';
 import { PersonsComponent } from './persons/persons.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PersonsComponent
+    PersonsComponent,
+    PersonInputComponent
   ],
   imports: [
     BrowserModule
